b83ee820e57fbe5d43130751c2fc94839585fdee
Ahmed_Adel <90941517+a7med3del1973@users.noreply.github.com>
2023-10-12 09:44:09 +0300

    Update firstTask.c

--- firstTask.c
+++ firstTask.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 #include <string.h>
-int main() {
- char userName[50]; 
-char password[50];
+#define ll long long
 
-    printf("\n Enter your username : ");
-    scanf("%s", userName); 
-    printf("\n Enter your password : ");
-    scanf("%s", password);
+struct User {
+    char userName[50];
+    char passWord[50];
+};
 
-    if (strcmp(userName, "Ahmed") == 0) { 
-        if (strcmp(password, "1973") == 0) {
-            printf("Login Successfully \n");
-        } else {
-            printf("\n Wrong Password \n"); 
-        }
-    } else {
-        printf("\n User Doesn't Exist \n"); 
+struct User users[100];
+ll usersCnt = 0;
+
+void userRegister() {
+    if (usersCnt >= 100) {
+        printf("This form is full... Contact the owner for more information.\n");
+        return;
     }
 
-    getchar(); 
+    printf("\nEnter your username: ");
+    scanf("%s", users[usersCnt].userName);
+    printf("\nEnter your password: ");
+    scanf("%s", users[usersCnt].passWord);
+    usersCnt++;
 
+    printf("Registered successfully\n");
+}
+
+void userLogin() {
+    char username[50];
+    char password[50];
+    printf("\nEnter your username: ");
+    scanf("%s", username);
+    printf("\nEnter your password: ");
+    scanf("%s", password);
+
+    for (ll i = 0; i < usersCnt; i++) {
+        if (strcmp(users[i].userName, username) == 0 && strcmp(users[i].passWord, password) == 0) {
+            printf("Login Successfully\n");
+            return;
+        }
+    }
+    printf("Something went wrong, try again\n");
+}
+
+int main() {
+    printf("Welcome, sir\nPress 1 to register\nPress 2 to login\n");
+    ll choesed = 0;
+    scanf("%lld", &choesed);
+
+    if (choesed == 1) {
+        userRegister();
+    } else if (choesed == 2) {
+        userLogin();
+    }
+
+    getchar();
     return 0;
 }
